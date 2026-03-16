b14266d91d86fa87444ce5c59ba2b752cf4d35e8
Nathan Grace <favouramaraci1@gmail.com>
2026-03-16 10:21:18 +0100

    feat:rectangle

---
+++ rectangle.js
@@ -0,0 +1,13 @@
+// Declare length = 12 and width = 7 .
+// Calculate and print:
+// The area of the rectangle
+// The perimeter of the rectangle
+
+let length = 12;
+let width = 7;
+
+let area = length * width;
+let perimeter = 2 * (length + width);
+
+console.log(`The area of the rectangle is: ${area}`);
+console.log(`The perimeter of the rectangle is: ${perimeter}`);
