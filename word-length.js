820f86f8b65eb5079e5f5f03a00e4d2d73f10c85
Nathan Grace <favouramaraci1@gmail.com>
2026-03-16 11:46:15 +0100

    feat:word-length

---
+++ word-length.js
@@ -0,0 +1,61 @@
+
+let word1 = "programming";
+
+if (word1.length < 5) {
+  console.log(word1 + " :Short"); 
+  } 
+  else if (word1 .length >= 5 && word1 .length <= 8) {
+    console.log(word1 + " :Medium"); 
+  }
+   else {
+    console.log(word1 + " :Long"); 
+  } 
+
+   word2 = "hi";
+
+if (word2.length < 5) {
+  console.log(word2 + " :Short"); 
+  } 
+  else if (word2 .length >= 5 && word2 .length <= 8) {
+    console.log(word2 + " :Medium"); 
+  }
+   else {
+    console.log(word2 + " :Long"); 
+  } 
+
+  word3 = "hello";
+
+if (word3.length < 5) {
+  console.log(word3 + " :Short"); 
+  } 
+  else if (word3 .length >= 5 && word1 .length <= 8) {
+    console.log(word3 + " :Medium"); 
+  }
+   else {
+    console.log(word3 + " :Long"); 
+  } 
+ 
+   word4 = "language";
+
+if (word4.length < 5) {
+  console.log(word4 + " :Short"); 
+  } 
+  else if (word4 .length >= 5 && word4 .length <= 8) {
+    console.log(word4 + " :Medium"); 
+  }
+   else {
+    console.log(word4 + " :Long"); 
+  } 
+ 
+ 
+  word5 = "JavaScript"; 
+  if (word5.length < 5) {
+  console.log(word5 + " :Short"); 
+    } 
+  else if (word5 .length >= 5 && word5 .length <= 8) {
+    console.log(word5 + " :Medium"); 
+  }
+   else {
+    console.log(word5 + " :Long"); 
+  }
+  
