e66532e7289ddc326da914fbcdbba5729e4e25d3
Nathan Grace <favouramaraci1@gmail.com>
2026-03-16 15:59:49 +0100

    feat:star-row

--- star-row.js
+++ star-row.js
@@ -1,10 +1,18 @@
-// Declare a variable rows = 5 .
-// Use a for loop to print a row of stars on each line, where every row has the same number
-// of stars equal to rows .
-// Expected output (rows = 5):
-// *****
-// *****
-// *****
-// *****
-// *****
-// Then change rows to 3 and run again.
+
+let rows = 5;
+
+for (let i = 1; i <= rows; i++) {
+    console.log("*".repeat(rows))
+}
+
+rows = 3;
+ for (let i = 1; i <= rows; i++) {
+    console.log("*".repeat(rows));
+ }
+
+
+
+
+
+ 
+ 
