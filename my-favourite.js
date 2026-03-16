9dd19b0622a626c9812d94852210c7d01b520fce
Nathan Grace <favouramaraci1@gmail.com>
2026-03-16 09:49:07 +0100

    feat:my-favourite

---
+++ my-favourite.js
@@ -0,0 +1,7 @@
+let favouriteColor = "Red"
+let favouriteFood = "Rice"
+let favouriteSport = "Basketball"
+let favouriteMovies = "American movie"
+
+console.log (`My favourite color is ${favouriteColor},I love eating fried ${favouriteFood}, I enjoy playing ${favouriteSport} and my favourite movie is ${favouriteMovies}`)
+
