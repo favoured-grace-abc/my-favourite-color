6d08b71c8269f4655f8ee5dd2a4e1b721e1c8a80
Nathan Grace <favouramaraci1@gmail.com>
2026-03-16 11:20:30 +0100

    feat:leap-year

--- leap-year.js
+++ leap-year.js
@@ -4,3 +4,38 @@
 // Write conditions to print whether the year is a leap year or not.
 
 let year = 2024;
+if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+  console.log(`${year}: is a leap year.`);
+} else {
+  console.log(`${year}: is not a leap year.`);
+}
+
+ year = 2000;
+if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+  console.log(`${year}: is a leap year.`);
+} else {
+  console.log(`${year}: is not a leap year.`);
+
+}
+
+ year = 1900;
+if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+  console.log(`${year}: is a leap year.`);
+} else {
+  console.log(`${year}: is not a leap year.`);
+}
+
+ year = 2100;
+if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+  console.log(`${year}: is a leap year.`);
+} else {
+  console.log(`${year}: is not a leap year.`);
+
+}
+
+ year = 2023;
+if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+  console.log(`${year}: is a leap year.`);
+} else {
+  console.log(`${year}: is not a leap year.`);
+}
